3343c04c7d5a0329d360c6932877e36bff09b2fe
Youngbae1126 <rcplaza2663@gmail.com>
2024-01-02 18:48:31 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MyHeader from './MyHeader';
+
+function App() {
+	return (
+		<div className="App">
+			<MyHeader />
+			<header className="App-header">
+				<h2>안녕 리액트</h2>
+			</header>
+		</div>
+	);
+}
+
+export default App;
