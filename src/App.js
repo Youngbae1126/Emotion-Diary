0a79139f4250f000cc08ea59369e6d79114cdffa
Youngbae1126 <rcplaza2663@gmail.com>
2024-01-04 00:15:06 +0900

    Components: 버튼 및 헤더 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,27 +1,62 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import RouteTest from './components/RouterTest';
-
 import Home from './pages/Home';
 import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
 
-import MyHeader from './MyHeader';
+// COMPONENTS
+import MyHeader from './components/MyHeader';
+import MyButton from './components/MyButton';
 
 function App() {
+	const env = process.env;
+	env.PUBLIC_URL = env.PUBLIC_URL || '';
+
 	return (
 		<BrowserRouter>
 			<div className="App">
-				<MyHeader />
+				<MyHeader
+					headText={'App'}
+					leftChild={
+						<MyButton
+							text={'왼쪽 버튼'}
+							onClick={() => {
+								alert('왼쪽 클릭');
+							}}
+						/>
+					}
+					rightChild={
+						<MyButton
+							text={'오른쪽 버튼'}
+							onClick={() => {
+								alert('오른쪽 클릭');
+							}}
+						/>
+					}
+				/>
+
+				{/* <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} /> */}
+
+				<h2>App.js</h2>
+				<MyButton
+					text={'버튼'}
+					onClick={() => alert('버튼 클릭')}
+					type={'positive'}
+				/>
+				<MyButton
+					text={'버튼'}
+					onClick={() => alert('버튼 클릭')}
+					type={'negative'}
+				/>
+				<MyButton text={'버튼'} onClick={() => alert('버튼 클릭')} />
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/new" element={<New />} />
 					<Route path="/edit" element={<Edit />} />
 					<Route path="/diary/:id" element={<Diary />} />
 				</Routes>
-				<RouteTest />
 			</div>
 		</BrowserRouter>
 	);
